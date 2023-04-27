c90cc36b753c634cb889fd3554cb4cbc961081cd
rhyun9584 <rhyun9584@naver.com>
2023-04-27 15:57:25 +0900

    first commit

---
+++ src/main/java/com/j1001000/mabicookbook/MabiCookBookSpringApplication.java
@@ -0,0 +1,13 @@
+package com.j1001000.mabicookbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MabiCookBookSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MabiCookBookSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/j1001000/mabicookbook/MabiCookBookSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j1001000.mabicookbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MabiCookBookSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
